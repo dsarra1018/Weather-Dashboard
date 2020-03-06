e09d40663451735bf28eea7b5cbb478029153694
Darren Sarra <adriansarra18@gmail.com>
2020-03-06 02:08:37 -0500

    Added a search bar and a listener.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,35 @@
-// ajax call
-let APIkey = "03d3a9bae74e45b5a5a50325200303";
-let loc = prompt("Enter a city: ");
-let queryURL = "https://api.weatherapi.com/v1/forecast.json?key=" + APIkey + "&q=" + loc + "&days=5";
+$(document).ready(function(){
+    // array of searches
+    let searches = [];
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response);
+
+
+    
+    // // ajax call
+    // let APIkey = "03d3a9bae74e45b5a5a50325200303";
+    // let searches = [];
+    // let queryURL = "https://api.weatherapi.com/v1/forecast.json?key=" + APIkey + "&q=baltimore&days=6";
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function(response){
+    //     console.log(response);
+    // })
+
+    // on click listener
+    $("#searchBtn").on("click", function(event){
+        event.preventDefault();
+
+        // grab the input from the textbox
+        let city = $("#citySearch").val().trim();
+
+        // add city into the array
+        if (searches.includes(city)) {
+            return false;
+        }
+        else {
+            searches.push(city);
+        }
+    })
 })
